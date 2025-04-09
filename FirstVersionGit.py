f2af16a257bfd0325881fd39cb0805260f97b8a6
Nestor Bolivar <nbolivar@unal.edu.co>
2025-04-09 15:23:00 -0500

    version modificada

---
+++ FirstVersionGit.py
@@ -0,0 +1,34 @@
+##Nombrar archivos
+#R1.  no caracartesres especiales
+
+# esto e s un comentario
+
+# primera variable de tipo String
+# Float
+# Int
+# booleano
+
+#declaracion de variable
+
+dato = input("ingrese un dato")
+
+
+# operaciones simplificadas
+## dato %= 2
+## SIgnos  <  >
+# F strings, concatenar un dato
+
+while(dato < 10 ):
+    if( dato % 2 == 0):
+        print("el numero es par se descarrta como psrimo  " , dato)
+        print(dato)
+    else:
+        print("numero primo")
+        print(dato)
+    
+    dato += 1
+
+
+
+print(dato)
+#print(dato)
